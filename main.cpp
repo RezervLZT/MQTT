3921d6f66ece3609c71db6a1e7fe0d78fb2baccc
stafe-box <81144919+stafe-box@users.noreply.github.com>
2023-12-05 14:37:38 +0500

    Added timer interrupt

--- main.cpp
+++ main.cpp
@@ -3,19 +3,25 @@
 #include <ArduinoMqttClient.h>
 #include <TroykaIMU.h>
 #include <TroykaMeteoSensor.h>
+#include <RPi_Pico_TimerInterrupt.h>
 
+bool timer_int(struct repeating_timer *t);  // Прототип функции обработчика прерываний
 
+const char SSID[] = "ssid";                 // Название сети
+const char SSIDPass[] = "pass";             // Пароль для сети
 
+const char Username[] = "username";         // логин указанный в MQTT-брокере
+const char Password[] = "password";         // пароль указанный в MQTT-брокере
 
-char ssid[] = "SSID_NAME"; // Название сети
-char pass[] = "SSID_PASS"; // Пароль для сети
+const char Brocker[] = "mqtt.dealgate.ru";  // Брокер
+const int Port = 1883;                      // Порт подключения брокера
 
-#define USERNAME "username" // username указанный в dealgate
-#define PASSWORD "password" // password указанный в dealgate
-
+const int Interval = 1000000 * 10;          // Интервал отправки данных в микросекундах(второе число - секунды)
+RPI_PICO_Timer ITimer(0);                   // Экземпляр таймера
 
 
 byte picoID[] = 
+//Идентификатор устройства
 {
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff,             //here must be 6 mac address byte
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //here must be 8 flash chip id byte
@@ -31,42 +37,50 @@ Barometer barometer(0x5D);
 TroykaMeteoSensor meteoSensor(0x44);
 
 
-const char  broker[] = "mqtt.dealgate.ru";
-int       port = 1883;
 String    tempTopic;
 String    pressTopic;
 String    humTopic;
 
-const long interval = 10000;
 unsigned long previousMillis = 0;
 
 void setup() 
 {
-  Serial.begin(9600);
+
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.print("Attempting to connect to WPA SSID: ");
-  Serial.println(ssid);
-  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+
+  Serial.begin(9600);
+  Serial.print("#t# - Attempting to connect to WPA SSID: ");
+  Serial.println(SSID);
+  while (WiFi.begin(SSID, SSIDPass) != WL_CONNECTED) 
+  {
     // failed, retry
     Serial.print(".");
     delay(500);
   }
-  WiFi.macAddress(picoID);
-  for (int i = 0; i < 32; i+=2) 
+  Serial.println("#t# - You're connected to the network");
+
+  pico_unique_board_id_t p;     //Структура с ID чипа памяти
+  pico_get_unique_board_id(&p);
+
+  WiFi.macAddress(picoID);      //Записывыаем MAC адресс в первые 6 байт идентификатора
+
+  for (int i = 0; i <8; i++) 
+    picoID[i+6] = p.id[i];      //Записываем ID чипа памяти в следующие 8 байт идентификатора
+
+  for (int i = 0; i < 32; i+=2) //Конвертируем ID в строку
   {
     char buf[2];
     sprintf(buf, "%02x", picoID[i/2]);
     picoIDs[i] = buf[0];
     picoIDs[i+1] = buf[1];
   }
+  Serial.print("#t# - picoID: ");
   
-  Serial.println("You're connected to the network");
-  Serial.print("picoID: ");
   Serial.println(picoIDs);
   Serial.println();
   
-  //generate topics
+  //Генерируем топики
   tempTopic = String(picoIDs);
   tempTopic += "/temperature";
   pressTopic = String(picoIDs);
@@ -75,84 +89,94 @@ void setup()
   humTopic += "/humidity";
 
 
-  //connect mqtt server
-  Serial.print("Attempting to connect to the MQTT broker: ");
-  Serial.println(broker);
+  //Подключаемся к брокеру
+  Serial.print("#t# - Attempting to connect to the MQTT broker: ");
+  Serial.println(Brocker);
   mqttClient.setKeepAliveInterval(50000);
   mqttClient.setId(picoIDs);
-  mqttClient.setUsernamePassword(USERNAME, PASSWORD);
-  if (!mqttClient.connect(broker, port)) {
-    Serial.print("MQTT connection failed! Error code = ");
+  mqttClient.setUsernamePassword(Username, Password);
+  if (!mqttClient.connect(Brocker, Port)) 
+  {
+    Serial.print("#t# - MQTT connection failed! Error code = ");
     Serial.println(mqttClient.connectError());
-    rp2040.reboot();
+    rp2040.reboot();  // Если произошла ошибка при подключении к бркеру перезагружаемся
   }
-
-  Serial.println("You're connected to the MQTT broker!");
+  Serial.println("#t# - You're connected to the MQTT broker!");
   Serial.println();
 
   barometer.begin();
-  Serial.println("LPS is ready");
+  Serial.println("#t# - LPS is ready");
   Serial.println();
 
   meteoSensor.begin();
-  Serial.println("SHT is ready");
+  Serial.println("#t# - SHT is ready");
   Serial.println();
+
+  if (ITimer.attachInterruptInterval(Interval, timer_int))
+  {
+    Serial.print(F("#t# - Starting ITimer OK, millis() = ")); Serial.println(millis());
+  }
+  else
+    Serial.println(F("#t# - Can't set ITimer. Select another Timer, freq. or timer"));
 
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop() {
+void loop() 
+{
+  
+}
 
-  mqttClient.poll();
+bool timer_int(struct repeating_timer *t)
+{
+  mqttClient.poll();  //Проверяем есть ли новые сообщения
 
   if (!wifiClient.connected() or !mqttClient.connected())
   {
-    rp2040.reboot();
+    Serial.println("#t# - Connection failed, rebooting");
+    rp2040.reboot();  //Если произошёл разрыв соединения перезагружаем плату
   }
-  if (millis() <= previousMillis)
-    previousMillis = millis();
 
-  if (millis() - previousMillis >= interval) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    // save the last time a message was sent
-    int state = meteoSensor.read();
-    float pressure = barometer.readPressurePascals();
-    float temperature = (barometer.readTemperatureC() + meteoSensor.getTemperatureC())/2.0;//тепмературу выдаём как среднее арифметическое между данными с LPS и SHT
-    float humidity = meteoSensor.getHumidity();
-    Serial.print("SHT state: "); 
-    Serial.println(state);
-    Serial.print("WIFI Status: "); 
-    Serial.println(wifiClient.connected());
-    Serial.print("Is mqtt connected: ");
-    Serial.println(mqttClient.connected());
-    previousMillis = millis();
+  digitalWrite(LED_BUILTIN, HIGH);
+  // save the last time a message was sent
+  int state = meteoSensor.read();
+  float pressure = barometer.readPressurePascals();
+  float temperature = (barometer.readTemperatureC() + meteoSensor.getTemperatureC())/2.0;//тепмературу выдаём как среднее арифметическое между данными с LPS и SHT
+  float humidity = meteoSensor.getHumidity();
 
-    Serial.print("Sending message to topic: ");
-    Serial.println(tempTopic);
-    Serial.println(temperature);
+  Serial.print("#t# - SHT state: "); 
+  Serial.println(state);
+  Serial.print("#t# - WIFI Status: "); 
+  Serial.println(wifiClient.connected());
+  Serial.print("#t# - Is mqtt connected: ");
+  Serial.println(mqttClient.connected());
 
-    Serial.print("Sending message to topic: ");
-    Serial.println(pressTopic);
-    Serial.println(pressure);
+  Serial.print("#t# - Sending message to topic: ");
+  Serial.println(tempTopic);
+  Serial.println(temperature);
 
-    Serial.print("Sending message to topic: ");
-    Serial.println(humTopic);
-    Serial.println(humidity);
+  Serial.print("#t# - Sending message to topic: ");
+  Serial.println(pressTopic);
+  Serial.println(pressure);
 
-    // send message, the Print interface can be used to set the message contents
-    mqttClient.beginMessage(tempTopic);
-    mqttClient.print(temperature);
-    mqttClient.endMessage();
+  Serial.print("#t# - Sending message to topic: ");
+  Serial.println(humTopic);
+  Serial.println(humidity);
+  Serial.println();
 
-    mqttClient.beginMessage(pressTopic);
-    mqttClient.print(pressure);
-    mqttClient.endMessage();
+  // send message, the Print interface can be used to set the message contents
+  mqttClient.beginMessage(tempTopic);
+  mqttClient.print(temperature);
+  mqttClient.endMessage();
 
-    mqttClient.beginMessage(humTopic);
-    mqttClient.print(humidity);
-    mqttClient.endMessage();
+  mqttClient.beginMessage(pressTopic);
+  mqttClient.print(pressure);
+  mqttClient.endMessage();
 
-    Serial.println();
-    digitalWrite(LED_BUILTIN, LOW);
-  }
+  mqttClient.beginMessage(humTopic);
+  mqttClient.print(humidity);
+  mqttClient.endMessage();
+
+  digitalWrite(LED_BUILTIN, LOW);
+  return true;
 }
